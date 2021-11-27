802850d2c5d545169aea0849c5b7ec53104b327e
Aaron <ajonaaronajon@yahoo.com>
2021-11-26 23:16:49 -0600

    able to create array of Scoop objects by type

---
+++ src/Scoop.js
@@ -0,0 +1,3 @@
+export const scoop = (name, location, description, missedPoint = null)=>{
+    return {name, location, description, missedPoint};
+};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { idea } from "./Idea.js"
+import { scoop } from "./Scoop.js"
 
 const createIdeas = ()=>{
     fetch('/assets/json/ideas.json')
@@ -10,7 +11,15 @@ const createIdeas = ()=>{
     });
 }
 
-createIdeas();
+const createScoops = fetch('/assets/json/scoops.json')
+    .then(response => response.json())
+    .then(scoops => {
+        const allScoops = parseScoops(scoops["Missable"], true).concat(
+                          parseScoops(scoops["Unmissable"], false));
+        console.log(allScoops);
+});
+
+//createIdeas();
 
 const parseIdeas = (chapters)=>{
     const ideas = [];
@@ -21,3 +30,17 @@ const parseIdeas = (chapters)=>{
     }
     return ideas;
 }
+
+const parseScoops = (scoops, isMissable) =>{
+    const scoopArr = [];
+
+       for(const currScoop in scoops){
+           if(isMissable)//missable
+                scoopArr.push(scoop(scoops[currScoop]["Name"], scoops[currScoop]["Location"],
+                scoops[currScoop]["Description"], scoops[currScoop]["Point at which you missed it"]));     
+           else //unmissable
+                scoopArr.push(scoop(scoops[currScoop]["Name"], scoops[currScoop]["Location"],
+                scoops[currScoop]["Description"]));
+       }
+       return scoopArr;
+};
